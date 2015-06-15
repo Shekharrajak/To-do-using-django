698ff2f1ae7e24c371424104f9d869f7c630b0d3
Shekhar Prasad Rajak <shekharstudy@ymail.com>
2015-06-15 15:58:19 +0530

    adding all files

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from models import todo
 
 # Create your views here.
+
+def index(request):
+
+	item =todo.objects.all()
+
+	return render_to_response('index.html',{'item':items})
