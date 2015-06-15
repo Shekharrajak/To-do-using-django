95a3b0cb69c523f7b1929055cebd1e20ad55efbf
Shekhar Prasad Rajak <shekharstudy@ymail.com>
2015-06-15 15:54:04 +0530

    adding all files

--- todo/settings.py
+++ todo/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-DATABASE_ENGINE = 'to-do.db'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo.core',
+    'todo',
 
 )
 
--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
 from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'todo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    (r'^admin/(.*)',admin.site.root),
+    url(r'^$','todo.core.view.index'),
 )
